1860f12a371f6329eb7a4cd60cdc657173532997
Jakub Ćwik <94370732+JakubCwik@users.noreply.github.com>
2023-02-17 20:02:39 +0100

    Zadania

---
+++ Potegi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Potegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int D = int.Parse(Console.ReadLine());
+            for (int i = 0; i < D; i++)
+            {
+                string tekst = Console.ReadLine();
+                string[] liczby = tekst.Split(' ');
+                int podstawa = int.Parse(liczby[0]);
+                podstawa = podstawa % 10;
+                int wykladnik = int.Parse((liczby[1]));
+                if (wykladnik > 0)
+                {
+                    if (wykladnik % 4 == 0)
+                    {
+                        wykladnik = 4;
+                    }
+                    else
+                    {
+                        wykladnik = wykladnik % 4;
+                    }
+
+
+                    int wynik = podstawa;
+                    for (int j = 1; j < wykladnik; j++)
+                    {
+                        wynik *= podstawa;
+                    }
+                    Console.WriteLine(wynik%10);
+                }
+                else
+                {
+                    Console.WriteLine(1);
+                }
+
+
+            }
+        }
+    }
+}
---
+++ ZabawneDodawanie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ZabawneDodawanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wynik = 0;
+            int licznik = 0;
+            int t;
+            int liczbaSprawdzona;
+            bool czyZakonczono;
+
+            string tt = Console.ReadLine();
+            bool czyKonwertuje = int.TryParse(tt, out t);
+            if (t>80)
+            {
+                return;
+            }
+            for(int i = 0; i < t; i++)
+            {
+                string liczba = Console.ReadLine();
+                czyKonwertuje = int.TryParse(liczba, out liczbaSprawdzona);
+                if(!czyKonwertuje || liczbaSprawdzona<1 || liczbaSprawdzona>80)
+                {
+                    return;
+                }
+                czyZakonczono = false;
+                licznik = 0;
+                while (czyZakonczono != true)
+                {
+                    string odwroconaLiczba = "";
+                    for (int j = liczba.Length - 1; j >= 0; j--)
+                    {
+                        odwroconaLiczba += liczba[j];
+                    }
+
+                    if (odwroconaLiczba == liczba)
+                    {
+                        Console.WriteLine(liczba + " " + licznik);
+                        czyZakonczono = true;
+                    }
+                    else
+                    {
+                        wynik = int.Parse(liczba) + int.Parse(odwroconaLiczba);
+                        licznik++;
+                        liczba = wynik.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
